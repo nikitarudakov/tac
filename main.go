01a7d987196b5f46382ed55739eb6cfe64fce51d
foxstudent106092 <nik.datascience@gmail.com>
2023-10-11 18:43:11 +0100

    refactor: reimport cmd package with new module name

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/nikitarudakov/Traffic-Analyst-CLI/cmd"
+import (
+	"github.com/nikitarudakov/tac/cmd"
+)
 
 func main() {
 	cmd.Execute()
