4cdaa84d423d00f806362c7a4f5b034bc8892ff1
foxstudent106092 <nik.datascience@gmail.com>
2023-10-14 16:52:06 +0100

    refactor: add structures for Input and Output

--- zipfile/fileutils.go
+++ zipfile/fileutils.go
@@ -5,6 +5,31 @@ import (
 	"strings"
 )
 
+// Input stores input data such as path and compression formats
+type Input struct {
+	path string
+	zip  bool
+}
+
+// Output stores output data such as path and create boolean
+// which is used while creating folder at path
+type Output struct {
+	path   string
+	create bool
+}
+
+// SetInput sets Input fields
+func (in *Input) SetInput(path string, zip bool) {
+	in.path = path
+	in.zip = zip
+}
+
+// SetOutput sets Output fields
+func (in *Output) SetOutput(path string, create bool) {
+	in.path = path
+	in.create = create
+}
+
 func getFolderNameWithoutExt(path string) string {
 	return strings.TrimSuffix(filepath.Base(path), filepath.Ext(path))
 }
